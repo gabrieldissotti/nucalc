c5127ef8b25ed9b373297d700df1a52c6c20d953
Gabriel Dissotti <gabrieldnrodrigues@gmail.com>
2020-12-17 20:13:35 -0300

    feat(app): changed rendering order

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -9,8 +9,8 @@ function MyApp({ Component, pageProps }) {
         <link rel="preconnect" href="https://fonts.gstatic.com" />
         <link href="https://fonts.googleapis.com/css2?family=IBM+Plex+Sans+Condensed:wght@400;500&display=swap" rel="stylesheet" />
       </Helmet>
-      <Component {...pageProps} />
       <GlobalStyle />
+      <Component {...pageProps} />
     </>
   )
 }

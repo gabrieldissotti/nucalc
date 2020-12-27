59a4adbf7def74e429306cfb4e84dd7b28d15f62
Gabriel Dissotti <gabrieldnrodrigues@gmail.com>
2020-12-27 13:14:43 -0300

    feat(google analytiics): added google analitics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,14 +1,48 @@
-import { Helmet } from 'react-helmet';
+import React, { useEffect } from 'react';
+import { Head } from 'next/document';
+import { useRouter } from "next/router";
+
+import * as GoogleAnalyticsAPI from "../apis/GoogleAnalyticsAPI";
 
 import '../styles/main.scss'
 
 function MyApp({ Component, pageProps }) {
+
+  const router = useRouter();
+
+  useEffect(() => {
+    const handleRouteChange = (url: URL) => {
+      GoogleAnalyticsAPI.pageview(url);
+    };
+    router.events.on("routeChangeComplete", handleRouteChange);
+    return () => {
+      router.events.off("routeChangeComplete", handleRouteChange);
+    };
+  }, [router.events]);
+
   return (
     <>
-      <Helmet>
+      <Head>
         <link rel="preconnect" href="https://fonts.gstatic.com" />
         <link href="https://fonts.googleapis.com/css2?family=IBM+Plex+Sans+Condensed:wght@400;500&display=swap" rel="stylesheet" />
-      </Helmet>
+
+        <script
+          async
+          src={`https://www.googletagmanager.com/gtag/js?id=${GoogleAnalyticsAPI.GA_TRACKING_ID}`}
+        />
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+                window.dataLayer = window.dataLayer || [];
+                function gtag(){dataLayer.push(arguments);}
+                gtag('js', new Date());
+                gtag('config', '${GoogleAnalyticsAPI.GA_TRACKING_ID}', {
+                  page_path: window.location.pathname,
+                });
+            `
+          }}
+        />
+      </Head>
       <Component {...pageProps} />
     </>
   )

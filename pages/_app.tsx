ecf0cd49a52a288129cae94051e76979f63c3120
Gabriel Dissotti <gabrieldnrodrigues@gmail.com>
2020-12-27 18:00:30 -0300

    feat(google analytics): added analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,23 @@
 import React, { useEffect } from 'react';
+import { useRouter } from "next/router";
 
-import * as GoogleAnalyticsAPI from "../apis/GoogleAnalyticsAPI";
+import * as gtag from "../apis/GoogleAnalyticsAPI";
 
 import '../styles/main.scss'
 import { Helmet } from 'react-helmet';
 
 function MyApp({ Component, pageProps }) {
+  const router = useRouter();
 
+  useEffect(() => {
+    const handleRouteChange = (url: URL) => {
+      gtag.pageview(url);
+    };
+    router.events.on("routeChangeComplete", handleRouteChange);
+    return () => {
+      router.events.off("routeChangeComplete", handleRouteChange);
+    };
+  }, [router.events]);
 
   return (
     <>
@@ -14,19 +25,16 @@ function MyApp({ Component, pageProps }) {
         <link rel="preconnect" href="https://fonts.gstatic.com" />
         <link href="https://fonts.googleapis.com/css2?family=IBM+Plex+Sans+Condensed:wght@400;500;600&display=swap" rel="stylesheet" />
 
-        <script
-          async
-          src={`https://www.googletagmanager.com/gtag/js?id=G-B88FCC8KDR`}
-        ></script>
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-B88FCC8KDR"></script>
         <script
           dangerouslySetInnerHTML={{
             __html: `
-                window.dataLayer = window.dataLayer || [];
-                function gtag(){dataLayer.push(arguments);}
-                gtag('js', new Date());
-                gtag('config', 'G-B88FCC8KDR', {
-                  page_path: /,
-                });
+            alert(123)
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+
+              gtag('config', 'G-B88FCC8KDR');
             `
           }}
         ></script>

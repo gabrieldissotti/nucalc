8f6eb21c88c01fa0b3e44d9170c9557ad64aef99
Gabriel Dissotti <gabrieldnrodrigues@gmail.com>
2020-12-28 16:50:27 -0300

    feat(meta tags): added meta tags to head

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,10 +1,17 @@
 import Document, { Html, Head, Main, NextScript } from "next/document";
 
+import Cover from '../assets/images/shared-cover.png';
 export default class MyDocument extends Document {
   render() {
     return (
       <Html>
         <Head>
+          <meta property="og:url"                content={window.location.href} />
+          <meta property="og:type"               content="website" />
+          <meta property="og:title"              content="Calcular rendimento com resgate planejado no Nubank" />
+          <meta property="og:description"        content="Você sabe quanto seu dinheiro está deixando de render?" />
+          <meta property="og:image" content={Cover} />
+          
           <link rel="preconnect" href="https://fonts.gstatic.com" />
           <link href="https://fonts.googleapis.com/css2?family=IBM+Plex+Sans+Condensed:wght@400;500;600&display=swap" rel="stylesheet" />
           

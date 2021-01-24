64e40173326d925d56a649fca5115ab97aa5e9cb
Gabriel Dissotti <gabrieldnrodrigues@gmail.com>
2021-01-24 11:33:42 -0300

    feat(metatags): updated meta

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -14,28 +14,28 @@ export default class MyDocument extends Document {
           <meta property="og:type" content="article" />
           <meta property="type" content="article" />
           <meta name="type" content="article" />
-          <meta property="og:title" content="Calculadora para Nubank" />
-          <meta property="title" content="Calculadora para Nubank" />
-          <meta name="title" content="Calculadora para Nubank" />
-          <meta property="og:description" content="Você sabe quanto seu dinheiro está deixando de render?" />
-          <meta property="description" content="Você sabe quanto seu dinheiro está deixando de render?" />
-          <meta name="description" content="Você sabe quanto seu dinheiro está deixando de render?" />
+          <meta property="og:title" content="NuCalc" />
+          <meta property="title" content="NuCalc" />
+          <meta name="title" content="NuCalc" />
+          <meta property="og:description" content="Calcular resgate planejado do Nubank" />
+          <meta property="description" content="Calcular resgate planejado do Nubank" />
+          <meta name="description" content="Calcular resgate planejado do Nubank" />
           <meta property="og:image" content={Cover} />
           <meta property="image" content={Cover} />
           <meta name="image" content={Cover} />
           <meta property="og:locale" content="pt_BR" />
-          <meta property="og:site_name" content="Calculadora para Nubank" />
+          <meta property="og:site_name" content="NuCalc" />
           <meta name="twitter:url" content="https://rendimento-nubank-resgate-planejado-1vhoxpb3s.vercel.app" />
-          <meta name="twitter:title" content="Calculadora para Nubank" />
-          <meta name="twitter:description" content="Você sabe quanto seu dinheiro está deixando de render?" />
+          <meta name="twitter:title" content="NuCalc" />
+          <meta name="twitter:description" content="Calcular resgate planejado do Nubank" />
           {/* END SEO */}
 
           {/* START PWA DATA */}
-          <meta name='application-name' content='Calculadora para Nubank' />
+          <meta name='application-name' content='NuCalc' />
           <meta name='apple-mobile-web-app-capable' content='yes' />
           <meta name='apple-mobile-web-app-status-bar-style' content='default' />
-          <meta name='apple-mobile-web-app-title' content='PWA App' />
-          <meta name='description' content='Você sabe quanto seu dinheiro está deixando de render?' />
+          <meta name='apple-mobile-web-app-title' content='NuCalc' />
+          <meta name='description' content='Calcular resgate planejado do Nubank' />
           <meta name='format-detection' content='telephone=no' />
           <meta name='mobile-web-app-capable' content='yes' />
           <meta name='msapplication-config' content='/icons/browserconfig.xml' />
@@ -47,21 +47,21 @@ export default class MyDocument extends Document {
           <link rel='icon' type='image/png' sizes='32x32' href='/icons/favicon-32x32.png' />
           <link rel='icon' type='image/png' sizes='16x16' href='/icons/favicon-16x16.png' />
           <link rel='manifest' href='/manifest.json' />
-          <link rel='mask-icon' href='/icons/safari-pinned-tab.svg' color='#5bbad5' />
+          <link rel='mask-icon' href='/icons/safari-pinned-tab.svg' color='#2B5797' />
           <link rel='shortcut icon' href='/icons/favicon.ico' />
               
           <meta name='twitter:card' content='summary' />
-          <meta name='twitter:url' content='https://rendimento-nubank-resgate-planejado.gabrieldissotti.vercel.app' />
-          <meta name='twitter:title' content='Calculadora para Nubank' />
-          <meta name='twitter:description' content='Você sabe quanto seu dinheiro está deixando de render?' />
-          <meta name='twitter:image' content='https://rendimento-nubank-resgate-planejado.gabrieldissotti.vercel.app/icons/android-chrome-192x192.png' />
+          <meta name='twitter:url' content='https://nucalc.vercel.app' />
+          <meta name='twitter:title' content='NuCalc' />
+          <meta name='twitter:description' content='Calcular resgate planejado do Nubank' />
+          <meta name='twitter:image' content='https://nucalc.vercel.app/icons/android-chrome-192x192.png' />
           <meta name='twitter:creator' content='@Gabriel46340705' />
           <meta property='og:type' content='website' />
-          <meta property='og:title' content='Calculadora para Nubank' />
-          <meta property='og:description' content='Você sabe quanto seu dinheiro está deixando de render?' />
-          <meta property='og:site_name' content='Calculadora para Nubank' />
-          <meta property='og:url' content='https://rendimento-nubank-resgate-planejado.gabrieldissotti.vercel.app' />
-          <meta property='og:image' content='https://rendimento-nubank-resgate-planejado.gabrieldissotti.vercel.app/icons/apple-touch-icon.png' />
+          <meta property='og:title' content='NuCalc' />
+          <meta property='og:description' content='Calcular resgate planejado do Nubank' />
+          <meta property='og:site_name' content='NuCalc' />
+          <meta property='og:url' content='https://nucalc.vercel.app' />
+          <meta property='og:image' content='https://nucalc.vercel.app/icons/apple-touch-icon.png' />
           {/* END PWA DATA */}
 
           
